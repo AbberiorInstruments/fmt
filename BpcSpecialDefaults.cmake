1f5c14e4ae9736342a8cfd8fad790c061b482dfa
Andreas Schönle <a.schoenle@abberior-instruments.com>
2017-05-18 09:30:14 +0200

    new Bpc package

---
+++ BpcSpecialDefaults.cmake
@@ -0,0 +1,7 @@
+
+#Ignored in ai-master anyhow
+#set(FMT_INSTALL CACHE BOOL "Generate the install target." FORCE ON)
+set(FMT_DOC CACHE BOOL "Generate the install target." FORCE OFF)
+set(FMT_TEST CACHE BOOL "Generate the test target." FORCE OFF)
+set(FMT_USE_CPP11 CACHE BOOL "Enable the addition of C++11 compiler flags." FORCE ON)
+set(BUILD_SHARED_LIBS CACHE BOOL "Build shared library instead of static." FORCE ON)
